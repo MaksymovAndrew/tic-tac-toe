65da27f7c6a11f480a4b7d7c70d7d41c2c09af32
Andrew <herobrinemakcimov@gmail.com>
2024-10-31 15:34:14 +0100

    added X and O

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
+import { useState } from "react";
+
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSelectSquare() {
+    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+  }
+
+  console.log(activePlayer);
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="PLAYER 1" symbol="X" />
-          <Player initialName="PLAYER 2  " symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="PLAYER 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="PLAYER 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        GAME BOARD
+
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          activePlayerSymbol={activePlayer}
+        />
       </div>
       LOGGING
     </main>
